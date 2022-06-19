e840d64295c80dc65fbd525458ca9aa5bf477057
thientran <thien.y.tran@sjsu.edu>
2022-06-19 14:41:09 -0700

    feat: Connect sqlite3 database

--- main.go
+++ main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"log"
 
+	"database/sql"
+
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	log.Println("Starting server...")
+
+	_, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		panic("Could not connect to database.db!")
+	}
+	log.Println("Database connected...!!!")
+
+	// rows, _ := database.Query("SELECT * FROM accounts")
+	// var accountId string
+	// var userId string
+	// var accountType string
+	// var balance string
+	// var status string
+
+	// for rows.Next() {
+	// 	rows.Scan(&accountId, &userId, &accountType, &balance, &status)
+	// 	log.Println(accountId + " | " + userId + " | " + accountType + " | " + balance + " | " + status)
+	// }
 
 	app := fiber.New()
 
