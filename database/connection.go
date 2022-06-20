7edc4d4478ba6611cebcfe8b75b0b45a4008d67a
thientran <thien.y.tran@sjsu.edu>
2022-06-19 18:14:50 -0700

    feat: Add statement to create users table after database connected

--- database/connection.go
+++ database/connection.go
@@ -6,11 +6,29 @@ import (
 )
 
 func Connect() {
-	_, err := sql.Open("sqlite3", "./database.db")
+	db, err := sql.Open("sqlite3", "./database.db")
 	if err != nil {
 		panic("Could not connect to database.db!")
 	}
 	log.Println("Database connected...!!!")
+
+	// Create users table
+	statement, _ := db.Prepare(`
+		CREATE TABLE IF NOT EXISTS users (
+			userID INTEGER PRIMARY KEY,
+			username VARCHAR(255) NOT NULL,
+			password VARCHAR(255) NOT NULL,
+			firstName VARCHAR(255),
+			lastName VARCHAR(255),
+			address VARCHAR(255),
+			city VARCHAR(255),
+			state VARCHAR(255),
+			zipCode INT, 
+			phoneNumber VARCHAR(255),
+			pin VARCHAR(9) NOT NULL
+		);
+	`)
+	statement.Exec()
 
 	// rows, _ := database.Query("SELECT * FROM accounts")
 	// var accountId string
